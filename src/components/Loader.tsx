8ddffccba7a522d0c07b6902396133a55a03e5ac
andyols <andyols.dev@gmail.com>
2021-04-03 23:17:53 -0500

    🔁 Improve Loader Re-Usability

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,6 +1,10 @@
 import { Spinner } from '@chakra-ui/react'
 import React from 'react'
 
-export const Loader: React.FC = () => {
-  return <Spinner size='xl' thickness='5px' speed='0.5s' placeSelf='center' />
+interface LoaderProps {
+  size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl'
+}
+
+export const Loader: React.FC<LoaderProps> = ({ size = 'xl' }) => {
+  return <Spinner size={size} thickness='5px' speed='0.5s' placeSelf='center' />
 }
